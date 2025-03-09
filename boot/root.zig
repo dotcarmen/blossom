678bf04f60dcefe8d2a7a69e8fa740b412120f29
Carmen <carmen@dotcarmen.dev>
2025-03-10 01:55:48 +0700

    open elf kernel from disk and verify

--- boot/root.zig
+++ boot/root.zig
@@ -33,10 +33,46 @@ pub fn main() uefi.Status {
 }
 
 fn loadOS() uefi.Status.Error!void {
+    const boot_services = uefi.system_table.boot_services.?;
+
+    // TODO: defer close protocols
+    const loaded_image = try boot_services.openProtocolSt(uefi.protocol.LoadedImage, uefi.handle);
+    const device = loaded_image.device_handle orelse return uefi.Status.Error.NotFound;
+    const fs = try boot_services.openProtocolSt(uefi.protocol.SimpleFileSystem, device);
+
+    var vol: *uefi.protocol.File = undefined;
+    try fs.openVolume(@ptrCast(&vol)).err();
+    defer _ = vol.close();
+
+    var kernel: *const uefi.protocol.File = undefined;
+    try vol.open(
+        &kernel,
+        unicode.utf8ToUtf16LeStringLiteral("efi\\boot\\blossomk"),
+        uefi.protocol.File.efi_file_mode_read,
+        uefi.protocol.File.efi_file_read_only,
+    ).err();
+    defer _ = kernel.close();
+
+    log.info("kernel file successfully opened", .{});
+    var info: struct { info: uefi.FileInfo, name: [32]u8 } = undefined;
+    var len: usize = @sizeOf(@TypeOf(info));
+    try kernel.getInfo(&uefi.FileInfo.guid, &len, @ptrCast(&info)).err();
+    var real_name: [32]u8 = undefined;
+    const utf16_name = info.info.getFileName();
+    len = 0;
+    while (utf16_name[len] != 0) len += 1;
+    const name_len = unicode.utf16LeToUtf8(&real_name, utf16_name[0..len]) catch return uefi.Status.Error.BadBufferSize;
+    log.info("file {s} size {x} ({x} on disk) bytes", .{
+        real_name[0..name_len],
+        info.info.file_size,
+        info.info.physical_size,
+    });
+
     log.info("blossom loaded. press any key to continue...", .{});
     var events = [_]uefi.Event{uefi.system_table.con_in.?.wait_for_key};
+    // TODO: idk lol
     var idx: usize = 0;
-    try uefi.system_table.boot_services.?.waitForEvent(1, &events, &idx).err();
+    try boot_services.waitForEvent(1, &events, &idx).err();
 }
 
 const panic = std.debug.FullPanic(panicFn);
